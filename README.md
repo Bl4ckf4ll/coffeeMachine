7df88632824cd42c0c1cf1fd9691242b03e7c30f
David Maravilla <bl4ckf4ll@gmail.com>
2018-09-28 13:41:45 -0600

    Add readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# Coffe Machine
+Coffe machine project implementing TDD with nodejs and [Jest](https://jestjs.io)
+
+### Install Dependencies
+```bash 
+npm run install
+```
+
+### To build tests
+```bash 
+ npm run test
+ ```
+
+### Build project
+```bash 
+npm run build 
+```
--- tests/CoffeeMachine.test.js
+++ tests/CoffeeMachine.test.js
@@ -1,48 +1,82 @@
 let CoffeeMachine = require('../App/CoffeeMachine');
 
 CoffeeMachine = new CoffeeMachine({
-    coffee: 10,
-    sugar: 10,
-    cups: {
-        small: 20,
-        medium: 10,
-        large: 5,
-    },
+  coffee: 10,
+  sugar: 10,
+  cups: {
+    small: 20,
+    medium: 10,
+    large: 5,
+  },
 });
 
 describe('Test coffe machine', () => {
-    describe('Test cups', () => {
-        test('Get small cup of coffee', () => {
-            const coffee = CoffeeMachine.get('small', 2, 2);
-            const expectedCoffee = {
-                type: 'small',
-                coffeeAmount: 2,
-                sugarAmount: 2,
-            };
-
-            expect(coffee).toMatchObject(expectedCoffee);
-        });
+  describe('Test cups are set', () => {
+    test('Get small cup of coffee', () => {
+      expect(CoffeeMachine.cups.small).toBeTruthy();
     });
 
     test('Get medium cup of coffee', () => {
-        const coffee = CoffeeMachine.get('medium', 2, 2);
-        const expectedCoffee = {
-            type: 'medium',
-            coffeeAmount: 2,
-            sugarAmount: 2,
-        };
-
-        expect(coffee).toMatchObject(expectedCoffee);
+      expect(CoffeeMachine.cups.medium).toBeTruthy();
     });
 
     test('Get large cup of coffee', () => {
-        const coffee = CoffeeMachine.get('large', 2, 2);
-        const expectedCoffee = {
-            type: 'large',
-            coffeeAmount: 2,
-            sugarAmount: 2,
-        };
-
-        expect(coffee).toMatchObject(expectedCoffee);
+      expect(CoffeeMachine.cups.large).toBeTruthy();
     });
-})
+  });
+
+  test('There is coffee', () => {
+    expect(CoffeeMachine.coffee.has()).toBeTruthy();
+  });
+
+  test('There is sugar', () => {
+    expect(CoffeeMachine.sugar.has()).toBeTruthy();
+  });
+
+  test('there are small cups', () => {
+    expect(CoffeeMachine.cups.small.has()).toBeTruthy();
+  });
+
+  test('there are medium cups', () => {
+    expect(CoffeeMachine.cups.medium.has()).toBeTruthy();
+  });
+
+  test('there are large cups', () => {
+    expect(CoffeeMachine.cups.large.has()).toBeTruthy();
+  });
+
+  test('Give sugar', () => {
+    const beforeAmount = CoffeeMachine.sugar.amount;
+
+    CoffeeMachine.sugar.give(2);
+
+    expect(CoffeeMachine.sugar.amount).toBeLessThan(beforeAmount);
+  });
+
+  test('Give Coffee', () => {
+    const beforeAmount = CoffeeMachine.coffee.amount;
+
+    CoffeeMachine.coffee.give(2);
+
+    expect(CoffeeMachine.coffee.amount).toBeLessThan(beforeAmount);
+  });
+
+  test('Give Cup', () => {
+    const beforeAmount = CoffeeMachine.cups.small.amount;
+
+    CoffeeMachine.cups.small.give(2);
+
+    expect(CoffeeMachine.cups.small.amount).toBeLessThan(beforeAmount);
+  });
+
+  test('Give full coffee', () => {
+    const coffee = CoffeeMachine.get('small', 2, 2);
+    const expectedCoffee = {
+      type: 'small',
+      coffeeAmount: 2,
+      sugarAmount: 2,
+    };
+
+    expect(coffee).toMatchObject(expectedCoffee);
+  });
+});
